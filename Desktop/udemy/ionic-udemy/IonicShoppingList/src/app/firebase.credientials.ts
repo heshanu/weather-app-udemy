b92030be6b56a4050fecc0214dfb75075bf9fd42
heshanu <heshanu97@gmail.com>
2021-04-10 13:28:24 +0530

    Added firebase module

---
+++ Desktop/udemy/ionic-udemy/IonicShoppingList/src/app/firebase.credientials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG={
+    apiKey: "AIzaSyDX37ao25jSw3vUjTVKjeQO_1GWHKj76t0",
+    authDomain: "shoppinglist-udemy-f0552.firebaseapp.com",
+    projectId: "shoppinglist-udemy-f0552",
+    storageBucket: "shoppinglist-udemy-f0552.appspot.com",
+    messagingSenderId: "94472888050",
+    appId: "1:94472888050:web:6e2301d309de5c2da9ba6b"
+  };
--- Desktop/udemy/ionic-udemy/IonicShoppingList/src/app/home/home.module.ts
+++ Desktop/udemy/ionic-udemy/IonicShoppingList/src/app/home/home.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
+//import { HomePage } from './home.page';
+import {Angular}
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import {IonicPageModule} from 'ionic-angular';//HomePageModule
+//import {HomePage} from "./home";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,IonicPageModule.forchild(HomePageModule)
   ],
-  declarations: [HomePage]
+  declarations: [HomePageModule]
 })
 export class HomePageModule {}
